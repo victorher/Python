2ee993495a3fe2926c2afc5f1e6581f6fab1ef62
Víctor Hernández <61486282+victorher@users.noreply.github.com>
2024-10-08 13:18:15 -0500

    Ejemplos de variables

--- Program.py
+++ Program.py
@@ -1,1 +1,22 @@
-print("Hola Mundo")
+# Primer hola mundo
+texto = "Primer ejemplo: "
+print(texto + "Hola Mundo")
+
+# Variables
+A = 13
+print("Variable: " + str(A))
+
+# Ejecutar numeros pares
+numPar = [1,2,3,4,5,6,7,8,9,10]
+resPar = [num for num in numPar if num % 2 == 0]
+print("Números pares: " + str(resPar))
+
+# Multiplicar cada  número por 2
+x = 2
+resMul = [num * x for num in numPar]
+print("Numeros multiplicados por", x,":", resMul)
+
+# Exponente de numero par
+y = 3
+resExp = [num ** y for num in numPar if num % 2 == 0]
+print("Números pares con exponente", y,":", resExp)
