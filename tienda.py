f86251177d0e7422dccf0cd3802cd0e81b0c85c8
Víctor Hernández <61486282+victorher@users.noreply.github.com>
2024-10-12 08:42:59 -0500

    Pedir nombres por input

--- tienda.py
+++ tienda.py
@@ -8,6 +8,16 @@ num_gatos = 8
 num_pajaros = 25
 total = num_perros + num_pajaros + num_gatos
 
+print('\nPor favor ingresa tu nombre')
+nombre = input()
+print('\nPor favor ingresa tu apellido')
+apellido = input()
+
+# Concatenación
+nombre_completo = nombre + ' ' + apellido
+
+print('Gracias por visitarnos,', nombre_completo)
+
 print("Actualmente contamos con:")
 print(" Perros:", num_perros, "\n", "Gatos:", num_gatos, "\n", "Pajaros:", num_pajaros)
 print(" En total tenemos", total, "animales")
