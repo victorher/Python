17fb87991ba1ac03adb4b93e1d64c47bc8e79009
Víctor Hernández <61486282+victorher@users.noreply.github.com>
2024-10-11 20:20:44 -0500

    Aprendizaje de nuevos registros

---
+++ tienda.py
@@ -0,0 +1,13 @@
+print("*******************************************")
+print("*********       BIENVENIDO A      *********")
+print("*********   TIENDA DE MASCOTAS    *********")
+print("*******************************************")
+
+num_perros = 10
+num_gatos = 8
+num_pajaros = 25
+total = num_perros + num_pajaros + num_gatos
+
+print("Actualmente contamos con:")
+print(" Perros:", num_perros, "\n", "Gatos:", num_gatos, "\n", "Pajaros:", num_pajaros)
+print(" En total tenemos", total, "animales")
